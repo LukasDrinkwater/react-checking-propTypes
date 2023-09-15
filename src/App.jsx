68e5876aaabe66430eb8d51ed4f4c58897531266
Lukas <lukasdrinkwater97@gmail.com>
2023-09-15 20:05:31 +0100

    initial commit and some examples

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "../src/styles/reset.css";
+import 
 
-// function App() {
-//   const [count, setCount] = useState(0);
 
-//   return <h1>Our first test</h1>;
-// }
 
 const App = () => {
-  const [heading, setHeading] = useState("Magnificent Monkeys");
-
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
+  
 
   return (
     <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
+     
     </>
   );
 };
---
+++ src/components/RenderName.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const RenderName = (props) => {
+  return <div>{props.name}</div>;
+};
+
+RenderName.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+RenderName.defaultProps = {
+  name: "Zack",
+};
+
+export default RenderName;
